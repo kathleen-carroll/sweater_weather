82066d3bafbda14525ac5a801fee4529db6e8045
Kathleen Carroll <khakicarroll@comcast.net>
2020-04-20 09:57:23 -0600

    Add route to antipode

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :forecast, only: [:index, :show]
       resources :backgrounds, only: [:index]
+      resources :antipode, only: [:index]
     end
   end
 end
