d3eb02d5bb18a19b0942eaa7c453a12ef003dddb
Kathleen Carroll <khakicarroll@comcast.net>
2020-04-20 10:38:55 -0600

    Refactor service methods to get normal forecast or antipode service

--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -1,10 +1,12 @@
 class WeatherService
   def self.get_forecast(location_info, search_term = nil)
     location = LocationService.get_coordinates(location_info)
+    raw_data = get_json(location)
+
     if search_term.nil?
-      get_json_objects(location)
+      get_forecast_object(raw_data, location)
     else
-      get_antipode(location, search_term)
+      get_antipode_object(raw_data, location, search_term)
     end
   end
 
@@ -22,16 +24,21 @@ class WeatherService
     Faraday.new(url: "https://api.openweathermap.org/data/2.5/onecall?lat=#{latitude}&lon=#{longitude}&appid=#{ENV['WEATHER_API_KEY']}&units=imperial")
   end
 
-  def self.get_json_objects(location_info)
+  def self.get_json(location_info)
     response = conn(location_info).get
     raw_data = JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.get_forecast_object(raw_data, location_info)
+    # response = conn(location_info).get
+    # raw_data = JSON.parse(response.body, symbolize_names: true)
 
     Forecast.new(raw_data, location_info[:results].first[:address_components])
   end
 
-  def self.get_antipode(location_info, search_term)
-    response = conn(location_info).get
-    raw_data = JSON.parse(response.body, symbolize_names: true)
+  def self.get_antipode_object(raw_data, location_info, search_term)
+    # response = conn(location_info).get
+    # raw_data = JSON.parse(response.body, symbolize_names: true)
 
     # require "pry"; binding.pry
     Antipode.new(raw_data, location_info[:results].first[:address_components], search_term)
