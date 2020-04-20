002491187ee78112944adfe9aa3c618ed05fedfc
Kathleen Carroll <khakicarroll@comcast.net>
2020-04-20 08:51:09 -0600

    Start test for background api

---
+++ spec/api/v1/backgrounds_request_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe 'Forecast Api' do
+  it 'sends a list of forecast' do
+
+    get '/api/v1/backgrounds?location=denver,co'
+
+    expect(response).to be_successful
+
+    forecast = JSON.parse(response.body)
+
+    expect(forecast["data"]['attributes']["current_weather"].count).to eq(9)
+  end
+end
