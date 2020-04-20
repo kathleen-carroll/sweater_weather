de678f880546cb9e8e98e613c51583f499263299
Kathleen Carroll <khakicarroll@comcast.net>
2020-04-20 11:10:07 -0600

    Refactor into location service to reverse geocode

--- app/controllers/api/v1/antipode_controller.rb
+++ app/controllers/api/v1/antipode_controller.rb
@@ -1,30 +1,15 @@
 class Api::V1::AntipodeController < ApplicationController
   def index
-    # location_info = LocationService.get_coordinates(params[:location])
-    #
-    # latitude = location_info[:results].first[:geometry][:location][:lat]
-    # longitude = location_info[:results].first[:geometry][:location][:lng]
-    #
-    #
-    # conn = Faraday.new(url: "http://amypode.herokuapp.com/api/v1/antipodes?") do |faraday|
-    #   faraday.headers["api_key"] = ENV['AMYPODE_API_KEY']
-    #   faraday.params["lat"] = latitude
-    #   faraday.params["long"] = longitude
-    # end
 
     json = AntipodeService.find_antipode_coords(params[:location])
-
 
     lat = json[:data][:attributes][:lat]
     lng = json[:data][:attributes][:long]
 
-    # json = JSON.parse(conn.get.body, symbolize_names: true)
-    # lat = json[:data][:attributes][:lat]
-    # lng = json[:data][:attributes][:long]
-
-    rev_geo_conn = Faraday.new(url: "https://maps.googleapis.com/maps/api/geocode/json?latlng=#{lat},#{lng}&key=#{ENV['GOOGLE_API_KEY']}")
-
-    geocode_raw = JSON.parse(rev_geo_conn.get.body, symbolize_names: true)
+    # rev_geo_conn = Faraday.new(url: "https://maps.googleapis.com/maps/api/geocode/json?latlng=#{lat},#{lng}&key=#{ENV['GOOGLE_API_KEY']}")
+    #
+    # geocode_raw = JSON.parse(rev_geo_conn.get.body, symbolize_names: true)
+    geocode_raw = LocationService.reverse_geocode(lat, lng)
     city_name = geocode_raw[:results].first[:address_components][2][:long_name]
     country = geocode_raw[:results].first[:address_components][3][:long_name]
 
--- app/services/location_service.rb
+++ app/services/location_service.rb
@@ -1,16 +1,37 @@
 class LocationService
   def self.get_coordinates(location)
-    get_json(location)
+    get_address_json(location)
+  end
+
+  def self.reverse_geocode(lat, long)
+    # require "pry"; binding.pry
+    get_latlong_json(lat, long)
   end
 
   private
 
-  def self.conn(location)
-    Faraday.new(url: "https://maps.googleapis.com/maps/api/geocode/json?address=#{location}&key=#{ENV["GOOGLE_API_KEY"]}")
+  def self.conn
+    # Faraday.new(url: "https://maps.googleapis.com/maps/api/geocode/json?address=#{location}&key=#{ENV["GOOGLE_API_KEY"]}")
+    Faraday.new(url: "https://maps.googleapis.com/maps/api/geocode/json?key=#{ENV["GOOGLE_API_KEY"]}")
   end
 
-  def self.get_json(location)
-    response = conn(location).get
+  def self.get_address_json(location)
+    response = conn.get do |params|
+      params.params["address"] = location
+    end
+
     JSON.parse(response.body, symbolize_names: true)
   end
+
+  def self.get_latlong_json(lat, long)
+    response = conn.get do |params|
+      params.params["latlng"] = "#{lat},#{long}"
+    end
+
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  # rev_geo_conn = Faraday.new(url: "https://maps.googleapis.com/maps/api/geocode/json?latlng=#{lat},#{lng}&key=#{ENV['GOOGLE_API_KEY']}")
+  #
+  # geocode_raw = JSON.parse(rev_geo_conn.get.body, symbolize_names: true)
 end
