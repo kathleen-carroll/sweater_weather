0fd220805b77a95511957985ec51e0a4381992f1
Kathleen Carroll <khakicarroll@comcast.net>
2020-04-19 22:55:13 -0600

    Update weather service

--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -1,6 +1,7 @@
 class WeatherService
   def self.get_forecast(location_info)
-    get_json(location_info)
+    location = LocationService.get_coordinates(location_info)
+    get_json_objects(location)
   end
 
   private
@@ -12,8 +13,10 @@ class WeatherService
     Faraday.new(url: "https://api.openweathermap.org/data/2.5/onecall?lat=#{latitude}&lon=#{longitude}&appid=#{ENV['WEATHER_API_KEY']}&units=imperial")
   end
 
-  def self.get_json(location_info)
+  def self.get_json_objects(location_info)
     response = conn(location_info).get
-    JSON.parse(response.body, symbolize_names: true)
+    raw_data = JSON.parse(response.body, symbolize_names: true)
+
+    Forecast.new(raw_data, location_info[:results].first[:address_components])
   end
 end
