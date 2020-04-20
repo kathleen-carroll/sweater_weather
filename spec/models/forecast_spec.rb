110f2f5ed3ff1d3df5321ed572e13fcc0bc427ca
Kathleen Carroll <khakicarroll@comcast.net>
2020-04-20 00:55:42 -0600

    Finish story 1A

---
+++ spec/models/forecast_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Forecast, type: :model do
+  it 'has attributes' do
+    # handle = 'kathleen-carroll'
+    # url = 'https://github.com/kathleen-carroll'
+    # id = 5940848
+    # info = {login: handle, html_url: url, id: id}
+    #
+    # follower = Follower.new(info)
+    #
+    # expect(follower.handle).to eq(handle)
+  end
+end
