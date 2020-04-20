4cac37ed795cf09c287e4e18569e194b271d8af9
Kathleen Carroll <khakicarroll@comcast.net>
2020-04-20 00:39:00 -0600

    Update test

--- spec/api/v1/forecast_request_spec.rb
+++ spec/api/v1/forecast_request_spec.rb
@@ -2,8 +2,6 @@ require 'rails_helper'
 
 RSpec.describe 'Forecast Api' do
   it 'sends a list of forecast' do
-    # num = 3
-    # create(:forecast)
 
     get '/api/v1/forecast?location=denver,co'
 
@@ -11,6 +9,10 @@ RSpec.describe 'Forecast Api' do
 
     forecast = JSON.parse(response.body)
 
-    expect(forecast["data"].count).to eq(num)
+    expect(forecast["data"]['attributes']["current_weather"].count).to eq(9)
+    expect(forecast["data"]['attributes']["hourly_weather"].first.count).to eq(3)
+    expect(forecast["data"]['attributes']["hourly_weather"].count).to eq(8)
+    expect(forecast["data"]['attributes']["daily_weather"].count).to eq(6)
+    expect(forecast["data"]['attributes']["daily_weather"].first.count).to eq(6)
   end
 end
