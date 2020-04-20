b3ef6a3882cc9b18b4bd759c1462f474e12423e9
Kathleen Carroll <khakicarroll@comcast.net>
2020-04-20 10:53:32 -0600

    Refactor and creat antipode service

--- app/controllers/api/v1/antipode_controller.rb
+++ app/controllers/api/v1/antipode_controller.rb
@@ -1,31 +1,32 @@
 class Api::V1::AntipodeController < ApplicationController
   def index
-    # require "pry"; binding.pry
-    params[:location]
+    # location_info = LocationService.get_coordinates(params[:location])
+    #
+    # latitude = location_info[:results].first[:geometry][:location][:lat]
+    # longitude = location_info[:results].first[:geometry][:location][:lng]
+    #
+    #
+    # conn = Faraday.new(url: "http://amypode.herokuapp.com/api/v1/antipodes?") do |faraday|
+    #   faraday.headers["api_key"] = ENV['AMYPODE_API_KEY']
+    #   faraday.params["lat"] = latitude
+    #   faraday.params["long"] = longitude
+    # end
 
-    location_info = LocationService.get_coordinates(params[:location])
-
-    latitude = location_info[:results].first[:geometry][:location][:lat]
-    longitude = location_info[:results].first[:geometry][:location][:lng]
+    json = AntipodeService.find_antipode_coords(params[:location])
 
 
-    conn = Faraday.new(url: "http://amypode.herokuapp.com/api/v1/antipodes?") do |faraday|
-      faraday.headers["api_key"] = ENV['AMYPODE_API_KEY']
-      faraday.params["lat"] = latitude
-      faraday.params["long"] = longitude
-    end
-
-    json = JSON.parse(conn.get.body, symbolize_names: true)
     lat = json[:data][:attributes][:lat]
     lng = json[:data][:attributes][:long]
+
+    # json = JSON.parse(conn.get.body, symbolize_names: true)
+    # lat = json[:data][:attributes][:lat]
+    # lng = json[:data][:attributes][:long]
 
     rev_geo_conn = Faraday.new(url: "https://maps.googleapis.com/maps/api/geocode/json?latlng=#{lat},#{lng}&key=#{ENV['GOOGLE_API_KEY']}")
 
     geocode_raw = JSON.parse(rev_geo_conn.get.body, symbolize_names: true)
     city_name = geocode_raw[:results].first[:address_components][2][:long_name]
     country = geocode_raw[:results].first[:address_components][3][:long_name]
-
-    # require "pry"; binding.pry
 
     forecast = WeatherService.get_forecast(city_name + ", " + country, params[:location])
 
---
+++ app/services/antipode_service.rb
@@ -0,0 +1,34 @@
+class AntipodeService
+
+  def self.find_antipode_coords(search_term)
+    # require "pry"; binding.pry
+    get_antipode(search_term)
+
+    # lat = json[:data][:attributes][:lat]
+    # lng = json[:data][:attributes][:long]
+  end
+
+  private
+
+  def self.get_antipode(search_term)
+    location_info = LocationService.get_coordinates(search_term)
+
+    latitude = location_info[:results].first[:geometry][:location][:lat]
+    longitude = location_info[:results].first[:geometry][:location][:lng]
+
+    get_json(latitude, longitude)
+  end
+
+  def self.conn(latitude,longitude)
+    Faraday.new(url: "http://amypode.herokuapp.com/api/v1/antipodes?") do |faraday|
+      faraday.headers["api_key"] = ENV['AMYPODE_API_KEY']
+      faraday.params["lat"] = latitude
+      faraday.params["long"] = longitude
+    end
+  end
+
+  def self.get_json(latitude, longitude)
+    JSON.parse(conn(latitude, longitude).get.body, symbolize_names: true)
+  end
+
+end
