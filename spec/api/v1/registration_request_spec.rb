de4a569547c1e15b7f225557d18750bbcba01a73
Kathleen Carroll <khakicarroll@comcast.net>
2020-04-20 13:46:03 -0600

    Create tests for user creation

--- spec/api/v1/registration_request_spec.rb
+++ spec/api/v1/registration_request_spec.rb
@@ -16,6 +16,25 @@ RSpec.describe 'Registration Api' do
 
     expect(user["data"]['attributes'].count).to eq(2)
     expect(user["data"]["type"]).to eq('users')
-    expect(user["data"]['attributes']["email"]).to eq(test_info["email"])
+    expect(user["data"]['attributes']["email"]).to eq(test_info[:email])
+  end
+
+  it 'sends registered user info' do
+    test_user = create(:user, email: "whatever@example.com")
+    test_info = {
+      "email": "whatever@example.com",
+      "password": "password",
+      "password_confirmation": "password"
+      }
+
+    post '/api/v1/users', params: test_info
+
+    expect(response.status).to eq(400)
+    expect(response).to_not be_successful
+
+    user = JSON.parse(response.body)
+
+    expect(user.count).to eq(1)
+    expect(user["errors"].first).to eq("Email has already been taken")
   end
 end
