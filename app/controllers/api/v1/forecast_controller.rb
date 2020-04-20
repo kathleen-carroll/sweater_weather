2542322cdc4cb0f58f8a60e58173fcd1ecdf0380
Kathleen Carroll <khakicarroll@comcast.net>
2020-04-19 22:56:22 -0600

    Update controller to render json

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,10 +1,11 @@
 class Api::V1::ForecastController < ApplicationController
   def index
-    location_info = LocationService.get_coordinates(params[:location])
+    # ForecastFacade.new(params[:location])
+    # location_info = LocationService.get_coordinates(params[:location])
 
-    weather_info = WeatherService.get_forecast(location_info)
-require "pry"; binding.pry
+    weather_info = WeatherService.get_forecast(params[:location])
 
+    render json: ForecastSerializer.new(weather_info)
     # require "pry"; binding.pry
     # Time.at(weather_info[:current][:dt])
     # Time.at(weather_info[:current][:dt]).in_time_zone(weather_info[:timezone])
