33f5882fc1e7bc7139f050c24594eceb4bf2e980
Kathleen Carroll <khakicarroll@comcast.net>
2020-04-20 20:16:05 -0600

    Finish story 4

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
       resources :antipode, only: [:index]
       resources :users, only: [:create]
       resources :sessions, only: [:create]
+      resources :road_trip, only: [:create]
     end
   end
 end
